1f2cf2b1a07248faaad31a182059e14a60854f82
Joseph Sharpe <joseph.sharpe@bt.com>
2021-05-10 16:33:01 +0100

    Made layout and styling changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :quizzes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root controller: "quizzes", action: "index"
+
+
 end
