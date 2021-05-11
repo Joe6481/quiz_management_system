dd7f6158f62a7401d3da1db913ca7966595f3cee
Joseph Sharpe <joseph.sharpe@bt.com>
2021-05-11 12:16:17 +0100

    Added CSS, finished form creation

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,68 +3,59 @@
 class QuizzesController < ApplicationController
   before_action :set_quiz, only: %i[show edit update destroy]
 
-  # GET /quizzes or /quizzes.json
   def index
     @quizzes = Quiz.all
+    @quiz = Quiz.new
+
+    generate_quesitons
   end
 
-  # GET /quizzes/1 or /quizzes/1.json
   def show; end
 
-  # GET /quizzes/new
-  def new
-    @quiz = Quiz.new
-  end
-
-  # GET /quizzes/1/edit
   def edit; end
 
-  # POST /quizzes or /quizzes.json
   def create
     @quiz = Quiz.new(quiz_params)
 
-    respond_to do |format|
-      if @quiz.save
-        format.html do redirect_to @quiz, notice: "Quiz was successfully created." end
-        format.json { render :show, status: :created, location: @quiz }
-      else
-        format.html do render :new, status: :unprocessable_entity end
-        format.json { render json: @quiz.errors, status: :unprocessable_entity }
-      end
+    if @quiz.save
+      redirect_to @quiz, notice: "'#{@quiz.title}' was successfully created."
+    else
+      render :edit
     end
   end
 
-  # PATCH/PUT /quizzes/1 or /quizzes/1.json
   def update
-    respond_to do |format|
-      if @quiz.update(quiz_params)
-        format.html do redirect_to @quiz, notice: "Quiz was successfully updated." end
-        format.json { render :show, status: :ok, location: @quiz }
-      else
-        format.html do render :edit, status: :unprocessable_entity end
-        format.json { render json: @quiz.errors, status: :unprocessable_entity }
-      end
+    if @quiz.update(quiz_params)
+      redirect_to @quiz, notice: "'#{@quiz.title}' was successfully updated."
+    else
+      render :edit
     end
   end
 
-  # DELETE /quizzes/1 or /quizzes/1.json
   def destroy
     @quiz.destroy
-    respond_to do |format|
-      format.html do redirect_to quizzes_url, notice: "Quiz was successfully destroyed." end
-      format.json { head :no_content }
-    end
+    redirect_to quizzes_path, notice: "'#{@quiz.title}' was successfully destroyed."
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_quiz
     @quiz = Quiz.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def quiz_params
-    params.require(:quiz).permit(:title, :description)
+    params.require(:quiz).permit(:title, :description,
+                                 questions_attributes: [:content, :id, {answers_attributes: [:option, :correct, :id]}])
+    
+    # ...questions_attributes: [:content, :id, :_destroy, {answers_attributes: [:option, :correct, :id, :_destroy]}])
+  end
+
+  def generate_quesitons
+    3.times do
+      question = @quiz.questions.build
+      3.times do
+        question.answers.build
+      end
+    end
   end
 end
