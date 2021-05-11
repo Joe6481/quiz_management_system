f68a0871af37220c39c28adcff85ab0e6d145ec6
Joseph Sharpe <joseph.sharpe@bt.com>
2021-05-11 17:17:56 +0100

    added name to User, made 3 new accounts with roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: {restricted: 0, view: 1, edit: 2}
 end
