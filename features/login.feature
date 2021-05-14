fea4ddeec9196169ff4a2f8f3bbc8abf7159920b
Joseph Sharpe <joseph.sharpe@bt.com>
2021-05-14 13:34:08 +0100

    Log in feature passsing

--- features/login.feature
+++ features/login.feature
@@ -2,15 +2,16 @@ Feature: Login
 
   Scenario: Logging into an existing user with valid credentials
     Given I am not logged in
-    When I log into an existing user
-    Then I see that I am logged in
+    When  I log into an existing user
+    Then  I see that I am logged in
 
   Scenario: Logging in with invalid credentials
     Given I am not logged in
-    When I log in with invalid credentials
-    Then I see that my login failed
+    When  I log in with invalid credentials
+    Then  I see that I am logged out
 
   Scenario: Logging out
-    Given I am logged in
-    When I logout
-    Then I see that I am logged out
+    Given I am not logged in
+    When  I log into an existing user
+    And   I log out
+    Then  I see that I am logged out
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -4,15 +4,36 @@ Given("I am not logged in") do
   visit "/"
 end
 
+#Given("I am logged into an user in the {string} role") do |role|
+  #@user = FactoryBot.create(:user, role: role)
+  #fill_in("user[password]", with: @user.password)
+  #fill_in("user_username", with: @user.username)
+  #click_on "Submit"
+  #visit "/"
+#end
+
 When("I log into an existing user") do
   @user = FactoryBot.create(:user, role: "edit")
-
   fill_in("user_username", with: @user.username)
   fill_in("user_password", with: @user.password)
   click_on "Log in"
   visit "/"
 end
 
+When('I log in with invalid credentials') do
+  fill_in("user_username", with: "Bad name")
+  fill_in("user_password", with: "Bad pass")
+  click_on "Log in"
+end
+
 Then("I see that I am logged in") do
   expect(page).to have_content("Log out")
+end
+
+Then('I see that I am logged out') do
+  expect(page).to have_content("Please log in below")
+end
+
+When("I log out") do
+  click_on 'Log out'
 end
